5c5f806b8c3c027825e987dc8a33db6722bfa104
Dhruv Bhatnagar <dhruvbhatnagar10@gmail.com>
2020-04-26 22:01:48 +0530

    ft: created main title and added custom colors in tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,56 +1,34 @@
 import React from 'react';
 import Head from 'next/head';
 
+// Images
+// import TapLogoSVG from '../assets/tap_logo.svg';
+
 const Home = () => (
-  <div className='text-sans'>
+  <div>
     <Head>
       <title>Home</title>
       <link rel='icon' href='/favicon.ico' />
     </Head>
 
-    <div className='w-full'>
-      <h1 className='m-0 w-full text-primary pt-20 leading-tight text-5xl text-center font-bold'>
-        NextJS Typescript Tailwind Starter
-      </h1>
-      <p className='text-center my-4 text-m bg-purp'>
-        To get started, edit <code>pages/index.tsx</code> and save to reload.
-      </p>
-
-      <div className='max-w-4xl mx-auto pt-20 py-auto pb-8 flex flex-row justify-around'>
-        <a
-          href='https://nextjs.org/docs'
-          className='pt-4 px-5 pb-6 w-64 text-left no-underline text-gray-800 border border-gray-400 hover:border-blue-500'
-        >
-          <h3 className='m-0 text-blue-500 text-lg font-bold'>
-            Documentation &rarr;
-          </h3>
-          <p className='m-0 pt-3 py-0 pb-0 text-sm text-gray-900'>
-            Learn more about Next.js in the documentation.
+    <div className='bg-dark-primary'>
+      <section className='h-screen w-full px-16 flex justify-between items-center'>
+        <div className='w-2/4'>
+          <div className='text-white text-6xl font-bold leading-none mb-6'>
+            <span className='text-light-pink'>Built</span> by the <br />{' '}
+            Community
+          </div>
+          <p className='text-gray-300 font-semibold leading-normal'>
+            The Abstract People is a New Delhi based community that deals in
+            human welfare, conducts social campaigns and indulges itself in Arts
+            and Cultural Events.
           </p>
-        </a>
-        <a
-          href='https://nextjs.org/learn'
-          className='pt-4 px-5 pb-6 w-64 text-left no-underline text-gray-800 border border-gray-400 hover:border-blue-500'
-        >
-          <h3 className='m-0 text-blue-500 text-lg font-bold'>
-            Next.js Learn &rarr;
-          </h3>
-          <p className='m-0 pt-3 py-0 pb-0 text-sm text-gray-900'>
-            Learn about Next.js by following an interactive tutorial!
-          </p>
-        </a>
-        <a
-          href='https://github.com/zeit/next.js/tree/master/examples'
-          className='pt-4 px-5 pb-6 w-64 text-left no-underline text-gray-800 border border-gray-400 hover:border-blue-500'
-        >
-          <h3 className='m-0 text-blue-500 text-lg font-bold'>
-            Examples &rarr;
-          </h3>
-          <p className='m-0 pt-3 py-0 pb-0 text-sm text-gray-900'>
-            Find other example boilerplates on the Next.js GitHub.
-          </p>
-        </a>
-      </div>
+        </div>
+        <div className='text-white'>
+          {/* <TapLogoSVG /> */}
+          LOGO
+        </div>
+      </section>
     </div>
   </div>
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 module.exports = {
   theme: {
-    extend: {},
-    textColor: {
-      primary: '#5850EC',
+    extend: {
+      colors: {
+        primary: '#5850EC',
+        'dark-primary': '#171A1B',
+        'dark-blue': '#170F59',
+        'light-blue': '#2F3676',
+        'light-pink': '#FF3297'
+      },
     },
+    textColor: (theme) => ({
+      ...theme('colors'),
+    }),
     backgroundColor: (theme) => ({
-      ...theme('textColor'),
+      ...theme('colors'),
     }),
   },
   variants: {},
