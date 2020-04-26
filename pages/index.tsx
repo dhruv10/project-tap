a4187a75697f05f68fe331f5022f2e1af85fe64d
Dhruv Bhatnagar <dhruvbhatnagar10@gmail.com>
2020-04-27 02:03:16 +0530

    ft: created section two - blue ovals

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,31 +5,36 @@ import Head from 'next/head';
 // import TapLogoSVG from '../assets/tap_logo.svg';
 
 const Home = () => (
-  <div>
+  <div className='bg-dark-primary'>
     <Head>
       <title>Home</title>
       <link rel='icon' href='/favicon.ico' />
     </Head>
 
-    <div className='bg-dark-primary'>
-      <section className='h-screen w-full px-16 flex justify-between items-center'>
-        <div className='w-2/4'>
-          <div className='text-white text-6xl font-bold leading-none mb-6'>
-            <span className='text-light-pink'>Built</span> by the <br />{' '}
-            Community
-          </div>
-          <p className='text-gray-300 font-semibold leading-normal'>
-            The Abstract People is a New Delhi based community that deals in
-            human welfare, conducts social campaigns and indulges itself in Arts
-            and Cultural Events.
-          </p>
+    <section className='h-screen w-full px-16 flex justify-between items-center'>
+      <div className='w-2/4'>
+        <div className='text-white text-6xl font-bold leading-none mb-6'>
+          <span className='text-light-pink'>Built</span> by the <br /> Community
         </div>
-        <div className='text-white'>
-          {/* <TapLogoSVG /> */}
-          LOGO
-        </div>
-      </section>
-    </div>
+        <p className='text-gray-300 font-semibold leading-normal'>
+          The Abstract People is a New Delhi based community that deals in human
+          welfare, conducts social campaigns and indulges itself in Arts and
+          Cultural Events.
+        </p>
+      </div>
+      <div className='text-white'>
+        {/* <TapLogoSVG /> */}
+        LOGO
+      </div>
+    </section>
+
+    <section className='h-screen w-full px-16 flex justify-between items-center'>
+      <div className='bg-dark-blue h-2/5 w-6/5 rounded-oval absolute -left-1/10 top-80vh z-3'></div>
+      <div className='bg-light-blue h-3/5 w-6/5 rounded-oval absolute -left-1/10 top-88vh z-3'></div>
+      <div className='bg-dark-blue h-half w-full absolute left-0 top-120vh z-2'></div>
+      <div className='bg-light-blue h-1/3 w-3/4 rounded-oval absolute -right-3/20 top-150vh z-3'></div>
+      <div className='bg-dark-primary h-1/3 w-3/5 rounded-oval absolute -left-17/100 top-160vh z-3'></div>
+    </section>
   </div>
 );
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,36 @@ module.exports = {
         'light-blue': '#2F3676',
         'light-pink': '#FF3297'
       },
+      width: {
+        '6/5': '120%'
+      },
+      height: {
+        '1/3': '33.33%',
+        '2/5': '40%',
+        '3/5': '60%',
+        'half': '50%'
+      },
+      borderRadius: {
+        oval: '50%'
+      },
+      inset: {
+        '-1/10': '-10%',
+        '-3/20': '-15%',
+        '-17/100': '-17%',
+        '80vh': '80vh',
+        '88vh': '88vh',
+        '120vh': '120vh',
+        '150vh': '150vh',
+        '160vh': '160vh',
+      },
+      rotate: {
+        7: '7deg',
+      },
+      zIndex: {
+        2: 2,
+        3: 3,
+        4: 4,
+      }
     },
     textColor: (theme) => ({
       ...theme('colors'),
