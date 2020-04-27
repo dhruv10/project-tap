0e7b2af71cc9694f437b3538c897021c2abfe900
Dhruv Bhatnagar <dhruvbhatnagar10@gmail.com>
2020-04-27 18:44:36 +0530

    bug: fixed Regex issue

--- postcss.config.js
+++ postcss.config.js
@@ -5,16 +5,16 @@ module.exports = {
     'autoprefixer',
     process.env.NODE_ENV === 'production'
       ? [
-          '@fullhuman/postcss-purgecss',
-          {
-            content: [
-              './pages/**/*.{js,jsx,ts,tsx}',
-              './components/**/*.{js,jsx,ts,tsx}',
-            ],
-            defaultExtractor: (content) =>
-              content.match(/[w-/:]+(?<!:)/g) || [],
-          },
-        ]
+        '@fullhuman/postcss-purgecss',
+        {
+          content: [
+            './pages/**/*.{js,jsx,ts,tsx}',
+            './components/**/*.{js,jsx,ts,tsx}',
+          ],
+          defaultExtractor: (content) =>
+            content.match(/[w\\-\\/:]+(?<!:)/g) || [],
+        },
+      ]
       : undefined,
   ],
 };
