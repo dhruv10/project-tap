aef4b635b1415d3f9ac3041bfb6a42f17fe43726
Dhruv Bhatnagar <dhruvbhatnagar10@gmail.com>
2020-04-27 17:25:17 +0530

    fix: rotation updates in section two ovals and body overflow fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,12 +29,14 @@ const Home = () => (
     </section>
 
     <section className='h-screen w-full px-16 flex justify-between items-center'>
-      <div className='bg-dark-blue h-2/5 w-6/5 rounded-oval absolute -left-1/10 top-80vh z-3'></div>
-      <div className='bg-light-blue h-3/5 w-6/5 rounded-oval absolute -left-1/10 top-88vh z-3'></div>
-      <div className='bg-dark-blue h-half w-full absolute left-0 top-120vh z-2'></div>
-      <div className='bg-light-blue h-1/3 w-3/4 rounded-oval absolute -right-3/20 top-150vh z-3'></div>
-      <div className='bg-dark-primary h-1/3 w-3/5 rounded-oval absolute -left-17/100 top-160vh z-3'></div>
+      <div className='bg-dark-blue h-half w-6/5 rounded-oval absolute -left-1/10 top-75vh rotate-4 z-3'></div>
+      <div className='bg-light-blue h-4/5 w-7/5 rounded-oval absolute -left-1/5 top-88vh -rotate-7 z-3'></div>
+      <div className='bg-dark-blue h-4/5 w-full absolute left-0 top-120vh z-2'></div>
+      <div className='bg-light-blue h-half w-3/4 rounded-oval absolute -right-1/5 top-170vh rotate-4 z-3'></div>
+      <div className='bg-dark-primary h-half w-3/5 rounded-oval absolute -left-1/120 top-180vh rotate-4 z-3'></div>
     </section>
+
+    <section className='h-screen w-full px-16 flex justify-between items-center'></section>
   </div>
 );
 
