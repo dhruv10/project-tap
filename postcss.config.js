f99fe55da2821d1f78fa2ac276d33b5c0be6a5d5
Dhruv Bhatnagar <dhruvbhatnagar10@gmail.com>
2020-05-05 19:33:14 +0530

    testing: removing purge

--- postcss.config.js
+++ postcss.config.js
@@ -3,18 +3,18 @@ module.exports = {
     'postcss-import',
     'tailwindcss',
     'autoprefixer',
-    process.env.NODE_ENV === 'production'
-      ? [
-        '@fullhuman/postcss-purgecss',
-        {
-          content: [
-            './pages/**/*.{js,jsx,ts,tsx}',
-            './components/**/*.{js,jsx,ts,tsx}',
-          ],
-          defaultExtractor: (content) =>
-            content.match(/[w\\-\\/:]+(?<!:)/g) || [],
-        },
-      ]
-      : undefined,
+    // process.env.NODE_ENV === 'production'
+    //   ? [
+    //     '@fullhuman/postcss-purgecss',
+    //     {
+    //       content: [
+    //         './pages/**/*.{js,jsx,ts,tsx}',
+    //         './components/**/*.{js,jsx,ts,tsx}',
+    //       ],
+    //       defaultExtractor: (content) =>
+    //         content.match(/[w\\-\\/:]+(?<!:)/g) || [],
+    //     },
+    //   ]
+    //   : undefined,
   ],
 };
