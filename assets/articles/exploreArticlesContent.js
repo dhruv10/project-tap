4f6a22d8a7232fd7ccb4c0048f688c6f41581a41
Prabhcharan Singh Sethi <prabhcharanss@gmail.com>
2020-08-12 18:39:26 +0530

    filtered

--- assets/articles/exploreArticlesContent.js
+++ assets/articles/exploreArticlesContent.js
@@ -1,5 +1,5 @@
 export const exploreArticlesContent = [
-<<<<<<< HEAD
+
   {
     'category': 'Arts & Entertainment',
     'topic': 'Fear',
@@ -71,88 +71,3 @@ export const exploreArticlesContent = [
     'img': '/images/2.jpeg',
   },
 ];
-=======
-    {
-        'category': 'Arts & Entertainment',
-        'topic': 'Tezos Commons',
-        'description': 'This is a test file, test check tezzos common, hail hydra This is a test file, test check tezzos common, hail hydra This is a test file, test check tezzos common, hail hydra',
-        'author': 'Nanak Singh Khurana',
-        'issueDate': 'May 14, 2020',
-        'timeToRead': '6 min read',
-        'img': '/images/one_eyed.jpg'
-    },
-    {
-        'category': 'Industry',
-        'topic': 'Impact of Coronavirus on Indian Economy',
-        'description': 'After every economic crisis there are 5 ways in which economy of a country can go...',
-        'author': 'Nanak Singh Khurana',
-        'issueDate': 'July 15, 2020',
-        'timeToRead': '4 min read',
-        'img': '/images/covid.jpg'
-    },
-    {
-        'category': 'Arts & Entertainment',
-        'topic': 'Pain is Positive',
-        'description': 'If you ever get to know the sadness behind the smiling faces, you would never believe in the reality of the smiles.',
-        'author': 'Prabhnoor Kaur',
-        'issueDate': 'July 15, 2020',
-        'timeToRead': '2 min read',
-        'img': '/images/pain.jpg'
-    },
-    {
-        'category': 'Innovation & Technology',
-        'topic': 'Tezos Commons',
-        'description': 'This is a test file, test check tezzos common, hail hydra This is a test file, test check tezzos common, hail hydra This is a test file, test check tezzos common, hail hydra',
-        'author': 'Nanak Singh Khurana',
-        'issueDate': 'May 14, 2020',
-        'timeToRead': '6 min read',
-        'img': '/images/one_eyed.jpg'
-    },
-    {
-        'category': 'Life',
-        'topic': 'Tezos Commons',
-        'description': 'This is a test file, test check tezzos common, hail hydra This is a test file, test check tezzos common, hail hydra This is a test file, test check tezzos common, hail hydra',
-        'author': 'Nanak Singh Khurana',
-        'issueDate': 'May 14, 2020',
-        'timeToRead': '6 min read',
-        'img': '/images/one_eyed.jpg'
-    },
-    {
-        'category': 'Life',
-        'topic': 'Tezos Commons',
-        'description': 'This is a test file, test check tezzos common, hail hydra This is a test file, test check tezzos common, hail hydra This is a test file, test check tezzos common, hail hydra',
-        'author': 'Nanak Singh Khurana',
-        'issueDate': 'May 14, 2020',
-        'timeToRead': '6 min read',
-        'img': '/images/one_eyed.jpg'
-    },
-    {
-        'category': 'Society',
-        'topic': 'Tezos Commons',
-        'description': 'This is a test file, test check tezzos common, hail hydra This is a test file, test check tezzos common, hail hydra This is a test file, test check tezzos common, hail hydra',
-        'author': 'Nanak Singh Khurana',
-        'issueDate': 'May 14, 2020',
-        'timeToRead': '6 min read',
-        'img': '/images/one_eyed.jpg'
-    },
-    {
-        'category': 'Innovation & Technology',
-        'topic': 'Tezos Commons',
-        'description': 'This is a test file, test check tezzos common, hail hydra This is a test file, test check tezzos common, hail hydra This is a test file, test check tezzos common, hail hydra',
-        'author': 'Nanak Singh Khurana',
-        'issueDate': 'May 14, 2020',
-        'timeToRead': '6 min read',
-        'img': '/images/one_eyed.jpg'
-    },
-    {
-        'category': 'Society',
-        'topic': 'Society',
-        'description': 'This is a test file, test check tezzos common, hail hydra This is a test file, test check tezzos common, hail hydra This is a test file, test check tezzos common, hail hydra',
-        'author': 'Nanak Singh Khurana',
-        'issueDate': 'May 14, 2020',
-        'timeToRead': '6 min read',
-        'img': '/images/one_eyed.jpg'
-    }
-
-]
->>>>>>> 7feddae78aba2eb14bf148320693a8ae46faac2c
