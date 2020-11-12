4e17a9cf655b5613f970a0fc0b31ced598aee9f9
Dhruv Bhatnagar <dhruvbhatnagar10@gmail.com>
2020-11-12 14:02:14 +0530

    bug: fixed

--- assets/homePageArticles.js
+++ assets/homePageArticles.js
@@ -3,18 +3,19 @@ export const homePageArticles = [
     heading: 'The FOAMViz Project',
     content: 'A suit of open source visualization tools over FOAM TCRs data.',
     img: '/images/4.jpeg',
-    url: '/foamviz',
+    url: 'foamviz',
   },
   {
     heading: 'Ulajhna nahi aata',
     content: 'If you ever get to know the sadness behind the smiling faces',
     img: '/images/5.jpeg',
-    url: '/pip',
+    url: 'pip',
   },
   {
     heading: 'THE FIRST DAY',
     content:
       'On an unwanted monday morning,i woke up having feelings mixed with nervousness and curoristy It was the first day of a new grade, My feet did stop, my brain did fade',
     img: '/images/6.jpeg',
+    url: 'icie',
   },
 ];
